535edb2c25e35bd388afa03d0bb23d18c97b2b12
alpo6259 <alpo6259@gmail.com>
2022-03-02 14:13:13 +0200

    add index.js

---
+++ JS/index.js
@@ -0,0 +1,32 @@
+`use strict`;
+
+// function pow(base, exponent) {
+//   if (exponent === 0) {
+//     return base;
+//   }
+//   return base * pow(base, exponent - 1);
+// }
+
+// console.log("pow(2,4) :>> ", pow(2, 2));
+
+// function myPow(base, exponent) {
+//   if (exponent === 0) return base;
+
+//   if (exponent < 0) return myPow(base, exponent + 1) / base;
+
+//   if (exponent > 0) return myPow(base, exponent - 1) * base;
+// }
+
+// console.log(" :>> ", myPow(2, -5));
+
+// function bracketWrapper(n) {
+//   if (n === 0) {
+//     return;
+//   }
+
+//   console.log("(");
+//   bracketWrapper(n - 1);
+//   console.log(")");
+// }
+
+// bracketWrapper(2);
